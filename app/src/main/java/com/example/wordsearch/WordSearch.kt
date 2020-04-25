f9c8ac9e4893aa216788d0343c6ed3f2b1077ac8
Yousif Majid <ymajid@uwaterloo.ca>
2020-04-25 15:48:50 -0400

    built create random grid functionality

--- app/src/main/java/com/example/wordsearch/WordSearch.kt
+++ app/src/main/java/com/example/wordsearch/WordSearch.kt
@@ -1,5 +1,55 @@
 package com.example.wordsearch
 
 class WordSearch {
+    val letters = "ABCDEFGHIJKLMOPQRSTUVWSYZ"
 
+    val wordList = listOf(
+        "Kotlin",
+        "Swift",
+        "Java",
+        "ObjectiveC",
+        "Variable",
+        "Mobile",
+        "Shopify",
+        "Fall",
+        "Challenge",
+        "Android"
+    )
+
+    val gridSize = 10
+
+    fun makeGrid(size: Int) {
+        val row = mutableListOf<Char>()
+        for (i in 1..size) {
+            row += ' '
+        }
+        val grid = mutableListOf<MutableList<Char>>()
+        for (i in 1..size) {
+            grid += row
+        }
+
+        fillSlots(grid)
+        printGrid(grid)
+    }
+
+    //TODO all of the rows this returns are the same
+    fun fillSlots(grid: MutableList<MutableList<Char>>) {
+        for (row in grid) {
+            val rowIterator = row.listIterator()
+            rowIterator.forEach { slot ->
+                if (slot == ' ') {
+                    rowIterator.set(letters.random())
+                }
+            }
+        }
+    }
+
+    fun printGrid(grid: MutableList<MutableList<Char>>) {
+        for (row in grid) {
+            for (slot in row) {
+                print(slot)
+            }
+            println("")
+        }
+    }
 }
